7e1edf8dcb1d99b3c77e9100f1c9ed8a7fce4cab
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-10-30 19:38:59 +0200

    Solved

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -16,16 +16,12 @@ public class FileReader {
             while (reader.ready()){
                 char symbol = (char) reader.read();
                 //if(symbol != '\r' && symbol != '\n' && symbol != ' ' && symbol != ':')
-                    result.append(symbol);
-                //System.out.println(result);
+                result.append(symbol);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //System.out.println(result);
-        //result = new StringBuilder(result.toString().replaceAll("[\r\n:]", ""));
         String[] split = result.toString().split("[ :\\n\\r]");
-        System.out.println(Arrays.toString(split));
         return new Profile(split[2], Integer.parseInt(split[6]), split[10], Long.parseLong(split[14]));
     }
 
